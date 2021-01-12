2831533b35332d2cb64b442502f644561b9b4379
Nikla <niklas.fischer@berlin.de>
2021-01-12 01:15:01 +0100

    adjust styling in dashboard

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -9,12 +9,21 @@ const Dashboard = ({data}) => {
       <h2 className="subHeader">Quick Stats</h2>
       <div className="quickStatsContainer">
         <div className="quickStat">
+          <h3 className="quickStatHeadline">Portfolio $ Value</h3>
+          <h2 className="quickStatNumber">80$</h2>
+        </div>
+        <div className="quickStat">
           <h3 className="quickStatHeadline">Portfolio Return</h3>
           <h2 className="quickStatNumber">80%</h2>
         </div>
-        <div className="quickStat"></div>
-        <div className="quickStat"></div>
-        <div className="quickStat"></div>
+        <div className="quickStat bestPerformer">
+          <h3 className="quickStatHeadline">Best Performer</h3>
+          <h2 className="quickStatNumber">80%</h2>
+        </div>
+        <div className="quickStat worstPerformer">
+          <h3 className="quickStatHeadline">Worst Performer</h3>
+          <h2 className="quickStatNumber">80%</h2>
+        </div>
       </div>
       <LineChart data={data} width={900} height={500} />
     </div>
