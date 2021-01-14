4bd640ef3c2f4f8e3b704b4436b090cd86e76524
Nikla <niklas.fischer@berlin.de>
2021-01-14 21:47:24 +0100

    get rid of unneccesary console log

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const getApiUrl = ticker =>
   `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${ticker}&outputsize=compact&apikey=${process.env.ALPHA_VANTAGE_KEY}`;
 
 app.get("/yahoo/:ticker", (req, res) => {
-  yahooFinance.historical(
-    {
+  yahooFinance.historical({
       symbol: req.params.ticker,
       from: "2021-01-01",
       to: new Date(),
@@ -17,7 +16,6 @@ app.get("/yahoo/:ticker", (req, res) => {
     (err, quotes) => {
       let dailyData = [];
       Object.keys(quotes).forEach(day => {
-        console.log(quotes[day]["date"]);
         dailyData.push([quotes[day]["date"], quotes[day]["open"]]);
       });
       res.json(dailyData);
