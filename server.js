2eceedbeb057b8bd813bf38afefd958d8df3b887
Nikla <niklas.fischer@berlin.de>
2021-01-14 20:18:15 +0100

    serve static files on serverport built from client folder

--- server.js
+++ server.js
@@ -30,4 +30,11 @@ app.get("/yahoo/:ticker", (req, res) => {
 });
 
 const PORT = process.env.PORT || 4000;
+
+// Serve static assets
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
 app.listen(PORT, console.log(`App running on port ${PORT} `));
